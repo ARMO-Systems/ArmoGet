6be6c087403186f3e03536abd7a311cab5341b00
Podlesnyy Andrey <podlesniy@gmail.com>
2014-04-15 17:21:24 +0400

    Rename Key

--- build.bat
+++ build.bat
@@ -10,6 +10,6 @@ if not "%PackageVersion%" == "" (
 )
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ArmoLib\ArmoLib.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Source\Main.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 $nugetExe pack Packs\ArmoLib.nuspec -NonInteractive
