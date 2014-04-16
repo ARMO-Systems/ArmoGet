ad56772d1b217da40168007d4f91e56a3c99598a
Podlesnyy Andrey <podlesniy@gmail.com>
2014-04-16 23:10:19 +0400

    d

--- build.bat
+++ build.bat
@@ -12,6 +12,6 @@ if not "%PackageVersion%" == "" (
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Source\Main.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
-%nuget% pack -BasePath Packs\ArmoLib.nuspec -NonInteractive -Version %version%
-%nuget% pack -BasePath Packs\HtmlDiff.nuspec -NonInteractive -Version %version%
-%nuget% pack -BasePath Packs\VariablesManager.nuspec -NonInteractive -Version %version%
+%nuget% pack -BasePath %SourcesPath%\Packs\ArmoLib.nuspec -NonInteractive -Version %version%
+%nuget% pack -BasePath %SourcesPath%\Packs\HtmlDiff.nuspec -NonInteractive -Version %version%
+%nuget% pack -BasePath %SourcesPath%\Packs\VariablesManager.nuspec -NonInteractive -Version %version%
