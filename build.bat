b7a74a0377652d55868d2863685247d0d27327d5
Podlesnyy Andrey <podlesniy@armo.ru>
2014-04-17 10:38:18 +0400

    3

--- build.bat
+++ build.bat
@@ -12,8 +12,9 @@ if not "%PackageVersion%" == "" (
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Source\Main.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
-echo %SourcesPath%
+set firstPartNuget=%nuget% pack %SourcesPath%\Packs\
+set lastPartNuget=.nuspec -BasePath %SourcesPath% -NonInteractive -Version %version%
 
-cmd /c %nuget% pack -BasePath %SourcesPath% %SourcesPath%\Packs\ArmoLib.nuspec -NonInteractive -Version %version%
-cmd /c %nuget% pack -BasePath %SourcesPath% %SourcesPath%\Packs\HtmlDiff.nuspec -NonInteractive -Version %version%
-cmd /c %nuget% pack -BasePath %SourcesPath% %SourcesPath%\Packs\VariablesManager.nuspec -NonInteractive -Version %version%
+cmd /c %firstPartNuget%ArmoLib%lastPartNuget%
+cmd /c %firstPartNuget%HtmlDiff%lastPartNuget%
+cmd /c %firstPartNuget%VariablesManager%lastPartNuget%
