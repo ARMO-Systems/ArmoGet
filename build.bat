35cb67c7b785b81df3d485fbe10fcf925e1c8220
Podlesnyy Andrey <podlesniy@armo.ru>
2014-04-24 15:12:41 +0400

    f

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
-REM Build
+%nuget% restore Source\Main.sln -NoCache -NonInteractive
 "%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" Source\Main.sln /p:Configuration="%config%" /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 cd Packs
