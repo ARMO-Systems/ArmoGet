b6af8e305bb4a6a06da2d2d283828d65c019032f
Ezhikov Vladimir <v-ezhikov@armo.ru>
2014-04-24 10:55:08 +0400

    Do

---
+++ Source/ArmoLib/LinqEx/Do.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArmoLib.LinqEx
+{
+    public static partial class LinqEx
+    {
+        public static TSource Do< TSource >( this TSource source, Action< TSource > action ) where TSource : class
+        {
+            if ( action == null )
+                throw new ArgumentNullException( "action" );
+
+            action( source );
+
+            return source;
+        }
+    }
+}
